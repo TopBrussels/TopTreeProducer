ddcb9fefac3248bfe2455ca5b5a19e69b9026acd
villella <>
2009-07-08 09:55:39 +0000

    *** empty log message ***

--- interface/TRootSpinCorrGen.h
+++ interface/TRootSpinCorrGen.h
@@ -9,6 +9,7 @@
 #include <vector>
 #include <iostream>
 #include <iomanip>
+#include "TLorentzVector.h"
 
 using namespace std;
 
@@ -34,6 +35,7 @@ public:
 	  cosThetaTLHel_(gevt.cosThetaTLHel_),
 	  cosThetaTBHel_(gevt.cosThetaTBHel_),
 	  cosThetaTQHel_(gevt.cosThetaTQHel_),
+	  cosPhi_(gevt.cosPhi_),
 	  topsZMFMass_(gevt.topsZMFMass_)
 	  {;}
 	
@@ -42,33 +44,17 @@ public:
 	Double_t cosThetaTLHel() const { return cosThetaTLHel_;}
 	Double_t cosThetaTBHel() const { return cosThetaTBHel_;}
 	Double_t cosThetaTQHel() const { return cosThetaTQHel_;}
+	Double_t cosPhi() const { return cosPhi_;}
 	Double_t topsZMFMass() const { return topsZMFMass_;}
 
 
 	void setcosThetaTLHel(Double_t cosThetaTLHel) { cosThetaTLHel_ = cosThetaTLHel; }
 	void setcosThetaTBHel(Double_t cosThetaTBHel) { cosThetaTBHel_ = cosThetaTBHel; }
 	void setcosThetaTQHel(Double_t cosThetaTQHel) { cosThetaTQHel_ = cosThetaTQHel; }
+	void setcosPhi(Double_t cosPhi) { cosPhi_ = cosPhi; }
 	void settopsZMFMass(Double_t topsZMFMass) { topsZMFMass_ = topsZMFMass; }
  
 	virtual TString typeName() const { return "TRootSpinCorrGen"; }
-
-
-	///	friend std::ostream& operator<< (std::ostream& stream, const TRootSpinCorrGen& genEvt) {
-	  /*		stream << "TRootSpinCorrGen - is ";
-	        if(genEvt.isTtBar()) stream <<"not ";
-		stream <<"Ttbar event ";
-		if(genEvt.isFullHadronic()) stream <<" FullHadronic ";
-		if(genEvt.isFullLeptonic()) stream <<" FullLeptonic ";
-		if(genEvt.isSemiLeptonic()) stream <<" SemiLeptonic ";
-		if(genEvt.semiLeptonicChannel()==kMuon) stream <<" muonic";
-		if(genEvt.semiLeptonicChannel()==kElec) stream <<" electronic";
-		if(genEvt.semiLeptonicChannel()==kTau) stream <<" tauonic";
-		stream << std::endl;
-		stream << "Nof ISR: "<< genEvt.ISR().size();
-		stream << "Nof Top radiations: "<< genEvt.leptonicDecayTopRadiation().size() + genEvt.hadronicDecayTopRadiation().size();
-		//stream << "TRootSpinCorrGen - Charge=" << setw(2) << jet.charge() << " (Et,eta,phi)=("<< setw(10) << jet.Et() <<","<< setw(10) << jet.Eta() <<","<< setw(10) << jet.Phi() << ")"
-		return stream;*/
-	//	};
 
 
 private:
@@ -76,10 +62,11 @@ private:
 	double cosThetaTLHel_;
 	double cosThetaTBHel_;
 	double cosThetaTQHel_;
+	double cosPhi_;
 	double topsZMFMass_ ; 
 
 	
-  ClassDef (TRootSpinCorrGen,1);
+  ClassDef (TRootSpinCorrGen,2);
 };
 
 #endif
