ddce3b29145d0fa4f1da3d534641e21c3c897690
ajafari <>
2010-03-31 19:35:07 +0000

    Ready to calculate the conversion variable and TriggerMatching

--- src/ElectronAnalyzer.cc
+++ src/ElectronAnalyzer.cc
@@ -9,7 +9,12 @@ ElectronAnalyzer::ElectronAnalyzer(const edm::ParameterSet& producersNames):verb
 	dataType_ = producersNames.getUntrackedParameter<string>("dataType","unknown");
 	electronProducer_ = producersNames.getParameter<edm::InputTag>("electronProducer");
         primaryVertexProducer_ = producersNames.getParameter<edm::InputTag>("primaryVertexProducer");
-        newId_ = producersNames.getParameter<bool>("electronNewId");
+        TrackLabel_ = producersNames.getParameter<edm::InputTag>("generalTrackLabel");
+        newId_ = producersNames.getUntrackedParameter<bool>("electronNewId",false);
+        doTriggerMatching = producersNames.getUntrackedParameter<bool>("electronTriggerMatching",false);
+        std::vector<std::string> tmp;
+        tmp.clear();
+        pathNames = producersNames.getUntrackedParameter<std::vector<std::string> >("triggerPaths",tmp); // electron trigger path names
 }
 
 ElectronAnalyzer::ElectronAnalyzer(const edm::ParameterSet& producersNames, const edm::ParameterSet& myConfig, int verbosity):verbosity_(verbosity)
@@ -19,14 +24,19 @@ ElectronAnalyzer::ElectronAnalyzer(const edm::ParameterSet& producersNames, cons
 	useMC_ = myConfig.getUntrackedParameter<bool>("doElectronMC");
 	runSuperCluster_ = myConfig.getUntrackedParameter<bool>("runSuperCluster",false);
         primaryVertexProducer_ = producersNames.getParameter<edm::InputTag>("primaryVertexProducer");
-        newId_ = producersNames.getParameter<bool>("electronNewId");
+        TrackLabel_ = producersNames.getParameter<edm::InputTag>("generalTrackLabel");
+        newId_ = producersNames.getUntrackedParameter<bool>("electronNewId",false);
+        doTriggerMatching = producersNames.getUntrackedParameter<bool>("electronTriggerMatching",false);
+        std::vector<std::string> tmp;
+        tmp.clear();
+        pathNames = producersNames.getUntrackedParameter<std::vector<std::string> >("triggerPaths",tmp); // electron trigger path names
 }
 
 ElectronAnalyzer::~ElectronAnalyzer()
 {
 }
 
-void ElectronAnalyzer::Process(const edm::Event& iEvent, TClonesArray* rootElectrons, EcalClusterLazyTools& lazyTools )
+void ElectronAnalyzer::Process(const edm::Event& iEvent, TClonesArray* rootElectrons, EcalClusterLazyTools& lazyTools, const edm::EventSetup& iSetup )
 {
 
 	unsigned int nElectrons=0;
@@ -65,8 +75,9 @@ void ElectronAnalyzer::Process(const edm::Event& iEvent, TClonesArray* rootElect
 			,electron->charge()
 		); 
 //=======================================
-                
+                std::cout<<"electron->isGsfCtfScPixChargeConsistent(): "<<electron->isGsfCtfScPixChargeConsistent()<<endl;
                 localElectron.setChargeInfo(electron->scPixCharge(),electron->isGsfCtfScPixChargeConsistent(),electron->isGsfScPixChargeConsistent(),electron->isGsfCtfChargeConsistent());
+                std::cout<<"localElectron.isGsfCtfScPixConsistent(): "<<localElectron.isGsfCtfScPixConsistent()<<endl;
                 localElectron.setSeedingInfo(electron->ecalDrivenSeed(), electron->trackerDrivenSeed());
                 localElectron.setTrackClusterMatching(electron->eSuperClusterOverP(),electron->eEleClusterOverPout(),electron->eSeedClusterOverP(),
                         electron->eSeedClusterOverPout(),electron->deltaEtaSuperClusterTrackAtVtx(),electron->deltaEtaSeedClusterTrackAtCalo(),
@@ -74,7 +85,9 @@ void ElectronAnalyzer::Process(const edm::Event& iEvent, TClonesArray* rootElect
                         electron->deltaEtaEleClusterTrackAtCalo());
 
                 //setTrackProperties:
+                std::cout<<"sqrt(electron->trackMomentumAtVtx().Mag2()): "<<sqrt(electron->trackMomentumAtVtx().Mag2())<<endl;
                 localElectron.setTrackMomentum(sqrt(electron->trackMomentumAtVtx().Mag2()));
+                std::cout<<"localElectron.trackMomentum(): "<<localElectron.trackMomentum()<<endl;
                 localElectron.setTrackMomentumAtCalo(sqrt(electron->trackMomentumAtCalo().Mag2()));
                 localElectron.setTrackMomentumAtElectronCluster( sqrt(electron->trackMomentumAtEleClus().Mag2()));
                 localElectron.setTrackMomentumAtVtxWithConstraint( sqrt(electron->trackMomentumAtVtxWithConstraint().Mag2()));
@@ -188,7 +201,24 @@ void ElectronAnalyzer::Process(const edm::Event& iEvent, TClonesArray* rootElect
 //                localElectron.setTriggerInfo(electron->getTriggerInfo());
 //                localElectron.setConversion(electron->isFromConversion());
 
+                 // Conversion:
+                //MagneticFiled at origin:
+                edm::ESHandle<MagneticField> magfield;
+                iSetup.get<IdealMagneticFieldRecord>().get(magfield);
+                GlobalPoint origin_point(0,0,0);
+                float Bfied = magfield.product()->inTesla(origin_point).mag();
 
+                edm::Handle<reco::TrackCollection> MyTrackHandle;
+                iEvent.getByLabel(TrackLabel_,MyTrackHandle);
+                double Dist = 9999.;
+                double DCot = 9999.;
+                reco::TrackRef partnerTrack = getConversionPartnerTrack(*electron,MyTrackHandle,Bfied,Dist,DCot);
+                localElectron.setConversion(partnerTrack.isNonnull());
+                // Trigger
+                //Filling with stupid numbers to avoid randoms in case of doTriggerMatching == false
+                std::vector<std::pair<std::string, bool> > tmpInput;
+                tmpInput.push_back(make_pair("NoName",false));
+                localElectron.setTriggerinfo(tmpInput);
 //=======================================
 
 		// Variables from reco::GsfTrack
@@ -231,6 +261,7 @@ void ElectronAnalyzer::Process(const edm::Event& iEvent, TClonesArray* rootElect
                             localElectron.setIDCiCHyperTight3(int(patElectron->electronID("eidCiCHyperTight3")));
                             localElectron.setIDCiCHyperTight4(int(patElectron->electronID("eidCiCHyperTight4")));                           
                         }
+                        
 			// Matched genParticle
 			if(useMC_)
 			{
@@ -242,10 +273,130 @@ void ElectronAnalyzer::Process(const edm::Event& iEvent, TClonesArray* rootElect
 				}
 
 			}
+                        //triggerMatching
+                        if(doTriggerMatching && pathNames.size() != 0){
+                            std::vector<std::pair<std::string, bool> > input;
+                            input.clear();
+                            for(uint trig = 0; trig < pathNames.size(); trig++){
+                                input.push_back(make_pair(pathNames.at(trig),(patElectron->triggerObjectMatchesByPath(pathNames.at(trig)).size() != 0)));
+                            }
+                            localElectron.setTriggerinfo(input);
+                        }
 
 		}
 
 		new( (*rootElectrons)[j] ) TRootElectron(localElectron);
 		if(verbosity_>2) cout << "   ["<< setw(3) << j << "] " << localElectron << endl;
+//		if(verbosity_>2) cout << "   ["<< setw(3) << j << "] " << ((TRootElectron*)rootElectrons->At(j))->isGsfCtfScPixConsistent() << endl;
+//		if(verbosity_>2) cout << "   ["<< setw(3) << j << "] " << ((TRootElectron*)rootElectrons->At(j))->trackMomentum() << endl;
+//		if(verbosity_>2) cout << "   ["<< setw(3) << j << "] " << ((TRootElectron*)rootElectrons->At(j))->fbrem() << endl;
+//		if(verbosity_>2) cout << "   ["<< setw(3) << j << "] " << ((TRootElectron*)rootElectrons->At(j))->isFromConversion() << endl;
+//		if(verbosity_>2) cout << "   ["<< setw(3) << j << "] " << electron->fbrem() << endl;
 	}
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//methods for conversion============================================
+const reco::Track* ElectronAnalyzer::getElectronTrack(const reco::GsfElectron& electron, const float minFracSharedHits){
+    if(electron.closestCtfTrackRef().isNonnull() && electron.shFracInnerHits() > minFracSharedHits)
+        return (const reco::Track*)electron.closestCtfTrackRef().get();
+    return (const reco::Track*)(electron.gsfTrack().get());
+
+}
+
+std::pair<double, double> ElectronAnalyzer::getConversionInfo(TLorentzVector trk1_p4,int trk1_q, float trk1_d0,TLorentzVector trk2_p4,int trk2_q, float trk2_d0,float bFieldAtOrigin){
+    double tk1Curvature = -0.3*bFieldAtOrigin*(trk1_q/trk1_p4.Pt())/100.;
+    double rTk1 = fabs(1./tk1Curvature);
+    double xTk1 = (1./tk1Curvature - trk1_d0)*cos(trk1_p4.Phi());
+    double yTk1 = (1./tk1Curvature - trk1_d0)*sin(trk1_p4.Phi());
+
+    double tk2Curvature = -0.3*bFieldAtOrigin*(trk2_q/trk2_p4.Pt())/100.;
+    double rTk2 = fabs(1./tk2Curvature);
+    double xTk2 = (1./tk2Curvature - trk2_d0)*cos(trk2_p4.Phi());
+    double yTk2 = (1./tk2Curvature - trk2_d0)*sin(trk2_p4.Phi());
+
+    double dist = sqrt(pow(xTk1-xTk2, 2) + pow(yTk1-yTk2 , 2));
+    dist = dist - (rTk1 + rTk2);
+
+    double dcot = 1/tan(trk1_p4.Theta()) - 1/tan(trk2_p4.Theta());
+
+    return std::make_pair(dist, dcot);
+}
+reco::TrackRef ElectronAnalyzer::getConversionPartnerTrack(const reco::GsfElectron& gsfElectron,
+					 const edm::Handle<reco::TrackCollection>& track_h,
+					 const float bFieldAtOrigin,
+					 double& Dist,
+					 double& DCot,
+					 const float maxAbsDist,
+					 const float maxAbsDCot,
+					 const float minFracSharedHits){
+
+    const reco::TrackRef el_ctftrack = gsfElectron.closestCtfTrackRef();
+    const TrackCollection *ctftracks = track_h.product();
+
+
+    const reco::Track* el_track = getElectronTrack(gsfElectron, minFracSharedHits);
+    int ctfidx = -999;
+    int el_q   = el_track->charge();
+    TLorentzVector el_tk_p4(el_track->px(), el_track->py(), el_track->pz(), el_track->p());
+    double el_d0 = el_track->d0();
+
+
+    if(el_ctftrack.isNonnull() && gsfElectron.shFracInnerHits() > minFracSharedHits)
+    ctfidx = static_cast<int>(el_ctftrack.key());
+
+    int tk_i = 0;
+    double mindR = 999;
+
+    //make a null Track Ref
+    TrackRef ctfTrackRef = TrackRef() ;
+
+    for(TrackCollection::const_iterator tk = ctftracks->begin(); tk != ctftracks->end(); tk++, tk_i++) {
+    //if the general Track is the same one as made by the electron, skip it
+        if((tk_i == ctfidx)  &&  (gsfElectron.shFracInnerHits() > minFracSharedHits))
+            continue;
+
+
+        TLorentzVector tk_p4(tk->px(), tk->py(), tk->pz(), tk->p());
+
+        //look only in a cone of 0.3
+
+        double dR = deltaR(el_tk_p4.Eta(), el_tk_p4.Phi(), tk_p4.Eta(), tk_p4.Phi());
+        if(dR > 0.3)
+            continue;
+
+        int tk_q = tk->charge();
+        double tk_d0 = tk->d0();
+
+        //the electron and track must be opposite charge
+        if(tk_q + el_q != 0)
+            continue;
+
+        std::pair<double, double> convInfo =  getConversionInfo(el_tk_p4, el_q, el_d0,tk_p4, tk_q, tk_d0,bFieldAtOrigin);
+
+        double dist = convInfo.first;
+        double dcot = convInfo.second;
+
+        if(fabs(dist) < maxAbsDist && fabs(dcot) < maxAbsDCot && dR < mindR) {
+            ctfTrackRef = reco::TrackRef(track_h, tk_i);
+            mindR = dR;
+            Dist = dist ;
+            DCot = dcot ;
+        }
+
+    }//track loop
+
+    return ctfTrackRef;
 }
