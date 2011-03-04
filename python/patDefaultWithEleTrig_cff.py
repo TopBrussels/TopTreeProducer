ec7aa8b7f86e6d44817850ec57075bd66c104fc1
pvmulder <>
2011-03-04 09:34:43 +0000

    switch off electron trigger matching

--- python/patDefaultWithEleTrig_cff.py
+++ python/patDefaultWithEleTrig_cff.py
@@ -9,26 +9,31 @@ from PhysicsTools.PatAlgos.triggerLayer1.triggerProducer_cff import *
 #patTrigger.processName      = 'HLT8E29'
 #patTriggerEvent.processName = 'HLT8E29'
 
-#Keep the matchingInfo for two paths: one physics and on backup
-electronTriggerMatchHLTEle15SWL1R = electronTriggerMatchHLTEle15LWL1R.clone()
-electronTriggerMatchHLTEle15SWL1R.maxDeltaR = 0.3
-electronTriggerMatchHLTEle15SWL1R.src = 'selectedPatElectrons'
-electronTriggerMatchHLTEle15SWL1R.pathNames = 'HLT_Ele15_SW_L1R',#backup
-electronTriggerMatchHLTEle10SWL1R = electronTriggerMatchHLTEle15SWL1R.clone()
-electronTriggerMatchHLTEle10SWL1R.pathNames = 'HLT_Ele10_SW_L1R',#physics
+#Keep the matchingInfo for two paths: one physics and one backup
+#electronTriggerMatchHLTEle15SWL1R = electronTriggerMatchHLTEle15LWL1R.clone()
+#electronTriggerMatchHLTEle15SWL1R.maxDeltaR = 0.3
+#electronTriggerMatchHLTEle15SWL1R.src = 'selectedPatElectrons'
+#electronTriggerMatchHLTEle15SWL1R.pathNames = 'HLT_Ele15_SW_L1R',#backup
+#electronTriggerMatchHLTEle10SWL1R = electronTriggerMatchHLTEle15SWL1R.clone()
+#electronTriggerMatchHLTEle10SWL1R.pathNames = 'HLT_Ele10_SW_L1R',#physics
 
 # keep matching only for electron
-patTriggerMatcher = cms.Sequence(electronTriggerMatchHLTEle10SWL1R+electronTriggerMatchHLTEle15SWL1R+electronTriggerMatchHLTDoubleEle5SWL1R)
+#patTriggerMatcher = cms.Sequence(electronTriggerMatchHLTEle10SWL1R+electronTriggerMatchHLTEle15SWL1R+electronTriggerMatchHLTDoubleEle5SWL1R)
 
 #change the matching sources in embedder
-cleanPatElectronsTriggerMatch.matches = 'electronTriggerMatchHLTEle10SWL1R','electronTriggerMatchHLTEle15SWL1R'
-cleanPatElectronsTriggerMatch.src = 'selectedPatElectrons'
+#cleanPatElectronsTriggerMatch.matches = 'electronTriggerMatchHLTEle10SWL1R','electronTriggerMatchHLTEle15SWL1R'
+#cleanPatElectronsTriggerMatch.src = 'selectedPatElectrons'
 
 # keep Embedding only for electron
-patTriggerMatchEmbedder = cms.Sequence(cleanPatElectronsTriggerMatch)
+#patTriggerMatchEmbedder = cms.Sequence(cleanPatElectronsTriggerMatch)
 
 # change sources. Otherwis it looks for muonMatches, etc.
-patTriggerEvent.patTriggerMatches = 'electronTriggerMatchHLTEle10SWL1R','electronTriggerMatchHLTEle15SWL1R'
+#patTriggerEvent.patTriggerMatches = 'electronTriggerMatchHLTEle10SWL1R','electronTriggerMatchHLTEle15SWL1R'
 
-patDefaultWithTrigger = cms.Sequence(patDefaultSequence* patTrigger * patTriggerMatcher * patTriggerMatchEmbedder * patTriggerEvent)
-patAddTrigger = cms.Sequence(patTrigger * patTriggerMatcher * patTriggerMatchEmbedder * patTriggerEvent)
+#patDefaultWithTrigger = cms.Sequence(patDefaultSequence* patTrigger * patTriggerMatcher * patTriggerMatchEmbedder * patTriggerEvent)
+#patAddTrigger = cms.Sequence(patTrigger * patTriggerMatcher * patTriggerMatchEmbedder * patTriggerEvent)
+
+patDefaultWithTrigger = cms.Sequence(patDefaultSequence)
+#patAddTrigger = cms.Sequence(patTrigger * patTriggerEvent)
+
+
