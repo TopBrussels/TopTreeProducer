ad69b0f81c9fb75c591f87aab2532d3eb058b7bd
blyweert <>
2011-03-08 15:00:09 +0000

    Removed obsolete stuff

--- python/TowerRemover_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-cleanTowers=cms.EDProducer("TowerRemover",
-			DREleTwr=cms.double(0.20),
-			electronCriteria=cms.string('pt > 20 & ((abs( eta ) < 1.4442) | (abs( eta ) > 1.560 & abs( eta ) < 2.4)) & electronID("eidRobustTight") & ((dr03TkSumPt+dr03EcalRecHitSumEt+dr03HcalTowerSumEt)/et)< 0.1'),
-			src = cms.InputTag("selectedPatElectrons")
-)
