ccb2ff7e7d2e815997e329314717df4784e90f14
mmaes <>
2011-04-15 09:30:01 +0000

    TChained the macro

--- tools/SanityCheck.cc
+++ tools/SanityCheck.cc
@@ -32,6 +32,7 @@
 #include <TCanvas.h>
 #include <TBranch.h>
 #include <TTree.h>
+#include <TChain.h>
 #include <TClonesArray.h>
 
 #include <fstream>
@@ -102,11 +103,16 @@ int main(int argc, char *argv[]){
 
   unsigned int nEvents = 0; 
 
+  if (verbosity > 0) cout << "Declaring TChains" << endl;
+  TChain* eventTree = new TChain("eventTree");
+  TChain* runTree = new TChain("runTree");
+
+
   for (int fileID=0; fileID < fileNames.size(); fileID++) {
   
-    if (verbosity > 0) cout << "Opening file " << fileNames.at(fileID) << endl;
+    if (verbosity > 0) cout << "Adding file " << fileNames.at(fileID) <<  endl;
 
-    TFile* f = TFile::Open(fileNames.at(fileID).c_str());
+    /*TFile* f = TFile::Open(fileNames.at(fileID).c_str());
     
     TTree* runTree = (TTree*) f->Get("runTree");
     TTree* eventTree = (TTree*) f->Get("eventTree");
@@ -117,256 +123,256 @@ int main(int argc, char *argv[]){
     
     TBranch* event_br = (TBranch *) eventTree->GetBranch("Event");
     TRootEvent* event = 0;
-    event_br->SetAddress(&event);
+    event_br->SetAddress(&event);*/
+
+    eventTree->Add(fileNames.at(fileID).c_str());
+    runTree->Add(fileNames.at(fileID).c_str());
     
     nEvents += eventTree->GetEntriesFast();
       
-    //TBranch* br;
-    //TClonesArray* objects;
-
-    for (int i=1; i<eventTree->GetListOfBranches()->GetEntriesFast(); i++) {
-      
-      TBranch * branch = (TBranch *)eventTree->GetListOfBranches()->At(i);
-      
-      TObject* obj = branch->GetListOfLeaves()->At(0);
-      
-      std::string ObjName = obj->GetName();
-      
-      string::size_type position = ObjName.find_last_of("_");
-      
-      std::string className = "";
-      
-      if (strstr(ObjName.c_str(),"CaloJet"))
-	className="TopTree::TRootCaloJet";
-      else if (strstr(ObjName.c_str(),"PFJet"))
-	className="TopTree::TRootPFJet";
-      else if (strstr(ObjName.c_str(),"JPTJet"))
-	className="TopTree::TRootJPTJet";
-      else if (strstr(ObjName.c_str(),"GenJet"))
-	className="TopTree::TRootGenJet";
-      else if (strstr(ObjName.c_str(),"MCParticles"))
-	className="TopTree::TRootMCParticle";
-      else if (strstr(ObjName.c_str(),"NPGenEvent"))
-	className="TopTree::TRootNPGenEvent";
-      else if (strstr(ObjName.c_str(),"GenEvent"))
-	className="TopTree::TRootGenEvent";
-      else if (strstr(ObjName.c_str(),"Muon"))
-	className="TopTree::TRootMuon";
-      else if (strstr(ObjName.c_str(),"Electron"))
-	className="TopTree::TRootElectron";
-      else if (strstr(ObjName.c_str(),"TCMET"))
-	className="TopTree::TRootMET";
-      else if (strstr(ObjName.c_str(),"CaloMET"))
-	className="TopTree::TRootCaloMET";
-      else if (strstr(ObjName.c_str(),"PFMET"))
-	className="TopTree::TRootPFMET";
-      else if (strstr(ObjName.c_str(),"MET"))
-	className="TopTree::TRootMET";
-      else if (strstr(ObjName.c_str(),"MHT"))
-	className="TopTree::TRootMHT";
-      else if (strstr(ObjName.c_str(),"PrimaryVertex"))
-	className="TopTree::TRootVertex";     
-    
-      //runinfos
-      
-      runTree->GetEvent(0);
-      
-      //if (fileID == 0 && verbosity > 3 && runInfos->hltInputTag() != "")
-      //	cout << "- " << "TopTree::TRootRun" << setw(5) << " -> " << "\"" << runInfos->hltInputTag() << "\""  << endl;
-      
-      // making some tests with the branch
-
-      //Declartion of Branches and TClonesArray
-
-      TBranch* br = (TBranch *) eventTree->GetBranch((ObjName.substr(0,position)).c_str());
-  
-      if (br) {
-	if (verbosity > 1) cout << "  Loaded Branch " << className << " " << ObjName.substr(0,position) << endl;
-	TClonesArray* objects = new TClonesArray(className.c_str(), 0);
-	br->SetAddress(&objects);
-
-	arrays[ObjName.substr(0,position)]=std::pair<std::string,TClonesArray*>(className,objects);
-
-      } else if (verbosity > 1) cerr << "  ERROR loading Branch " << className << " " << ObjName.substr(0,position) << endl;
-	
-    }
-
-    for(unsigned int ievt=0; ievt<eventTree->GetEntriesFast(); ievt++) {
-    //for(unsigned int ievt=0; ievt<8000; ievt++) {
-      eventTree->GetEvent(ievt);
-      runTree->GetEvent(0);
-
-      if (verbosity > 0 && ievt % 1000 == 0) std::cout<<"Processing the "<<ievt<<"th event." << flush<<"\r";
-
-
-      for (std::map<std::string, std::pair<std::string,TClonesArray*> >::const_iterator it=arrays.begin(); it != arrays.end(); ++it) {
-
-	//cout << it->first << " " << it->second.first << " " << it->second.second->GetEntries() << endl;
-
-	//
-	// define global plots we want to see
-	//
-
-	string hist="";
-
-	hist="_size";
-	if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),100,0,100);
-
-	for (unsigned int o=0; o<it->second.second->GetEntries(); o++) {
-
-	  // because c++ it not flexible we need to write the elaborate code below.... Sorry:'(
-
-	  //
-	  //
-	  // Dear object developer:) You can find some sample code here to add plots so all your changes will happen below this comment.....
-	  //
-	  //
-
-	  //
-	  // HLT
-	  //
-
-	  /* NO IMPLEMENTATION YET */
-
-	  //
-	  // PRIMARY VERTEX
-	  //
-
-	  if (strstr(it->second.first.c_str(),"TopTree::TRootVertex")) {
-
-	    if(o==0) histos[it->first+"_size"]->Fill(it->second.second->GetEntries());
-
-	  }
-
-	  //
-	  // GenEvent
-	  //
-
-	  else if (strstr(it->second.first.c_str(),"TopTree::TRootGenEvent") || strstr(it->second.first.c_str(),"TopTree::TRootNPGenEvent")) {
-
-	    if(o==0) histos[it->first+"_size"]->Fill(it->second.second->GetEntries());
-
-	  }
-
-	  //
-	  // MCParticles
-	  //
-
-	  else if (strstr(it->second.first.c_str(),"TopTree::TRootMCParticle")) {
-
-	    if(o==0) histos[it->first+"_size"]->Fill(it->second.second->GetEntries());
-
-	  }
-
-	  //
-	  // JETS
-	  //
-
-	  // now putting all jet types together, these can be addressed seperately in the future if needed
-	  else if (strstr(it->second.first.c_str(),"TopTree::TRootGenJet") || strstr(it->second.first.c_str(),"TopTree::TRootCaloJet") || strstr(it->second.first.c_str(),"TopTree::TRootPFJet") || strstr(it->second.first.c_str(),"TopTree::TRootJPTJet") ){
-	    
-	    if(o==0) histos[it->first+"_size"]->Fill(it->second.second->GetEntries());
-
-	    // some local plots
-	    hist="_pt";
-	    if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),250,0,500);
-	    hist="_eta";
-	    if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),300,-6,6);	  
-	    hist="_phi";
-	    if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),250,-5,5);
-	  
-	    histos[it->first+"_pt"]->Fill(((TRootJet*)it->second.second->At(o))->Pt());
-	    histos[it->first+"_eta"]->Fill(((TRootJet*)it->second.second->At(o))->Eta());
-	    histos[it->first+"_phi"]->Fill(((TRootJet*)it->second.second->At(o))->Phi());
-		     
-	  }
-	  
-	  // 
-	  // MET
-	  //
-
-	  // now putting all met types together, these can be addressed seperately in the future if needed
-	  else if (strstr(it->second.first.c_str(),"TopTree::TRootCaloMET") || strstr(it->second.first.c_str(),"TopTree::TRootPFMET") || strstr(it->second.first.c_str(),"TopTree::TRootMET") ){
-	    
-	    if(o==0) histos[it->first+"_size"]->Fill(it->second.second->GetEntries());
-
-	    // some local plots
-	    hist="_Et";
-	    if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),250,0,500);
-	  
-	    histos[it->first+"_Et"]->Fill(((TRootMET*)it->second.second->At(o))->Et());
-
-		     
-	  }
-
-	  //
-	  // MUON
-	  //
-	  
-	  else if (strstr(it->second.first.c_str(),"TopTree::TRootMuon")){
-	    
-	    if(o==0) histos[it->first+"_size"]->Fill(it->second.second->GetEntries());
-
-	    // some local plots
-	    hist="_pt";
-	    if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),250,0,500);
-	    hist="_eta";
-	    if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),300,-6,6);	  
-	    hist="_phi";
-	    if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),250,-5,5);
-	  
-	    histos[it->first+"_pt"]->Fill(((TRootMuon*)it->second.second->At(o))->Pt());
-	    histos[it->first+"_eta"]->Fill(((TRootMuon*)it->second.second->At(o))->Eta());
-	    histos[it->first+"_phi"]->Fill(((TRootMuon*)it->second.second->At(o))->Phi());
-	  
-	  }
-
-	  //
-	  // ELECTRON
-	  //
-	  
-	  else if (strstr(it->second.first.c_str(),"TopTree::TRootElectron")){
-	    
-	    if(o==0) histos[it->first+"_size"]->Fill(it->second.second->GetEntries());
-
-	    // some local plots
-	    hist="_pt";
-	    if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),250,0,500);
-	    hist="_eta";
-	    if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),300,-6,6);	  
-	    hist="_phi";
-	    if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),250,-5,5);
-	  
-	    histos[it->first+"_pt"]->Fill(((TRootElectron*)it->second.second->At(o))->Pt());
-	    histos[it->first+"_eta"]->Fill(((TRootElectron*)it->second.second->At(o))->Eta());
-	    histos[it->first+"_phi"]->Fill(((TRootElectron*)it->second.second->At(o))->Phi());
-	  
-	  }
-
-	}
-
-	//delete it->second.second;
-      }
-
-    }
-
-    cout << endl;
-   
-    for (std::map<std::string, std::pair<std::string,TClonesArray*> >::const_iterator it=arrays.begin(); it != arrays.end(); ++it)
-      delete it->second.second;
-
-    //delete obj;
-    //delete br;
-
-    delete event_br;
-    delete run_br;
-
-    delete eventTree;
-    delete runTree;
-
-    //f->Close();
-
   }
+
+  TRootRun* runInfos = new TRootRun();
+  runTree->SetBranchStatus("runInfos*",1);
+  runTree->SetBranchAddress("runInfos",&runInfos);
+
+  TRootEvent* event = new TRootEvent();
+  eventTree->SetBranchStatus("Event*",1);
+  eventTree->SetBranchAddress("Event",&event);
+
+  if (verbosity > 0) cout << "Searching the TBranches" << endl;
+
+  for (int i=1; i<eventTree->GetListOfBranches()->GetEntriesFast(); i++) {
+      
+    TBranch * branch = (TBranch *)eventTree->GetListOfBranches()->At(i);
+      
+    TObject* obj = branch->GetListOfLeaves()->At(0);
+    
+    std::string ObjName = obj->GetName();
+    
+    string::size_type position = ObjName.find_last_of("_");
+    
+    std::string className = "";
+    
+    if (strstr(ObjName.c_str(),"CaloJet"))
+      className="TopTree::TRootCaloJet";
+    else if (strstr(ObjName.c_str(),"PFJet"))
+      className="TopTree::TRootPFJet";
+    else if (strstr(ObjName.c_str(),"JPTJet"))
+      className="TopTree::TRootJPTJet";
+    else if (strstr(ObjName.c_str(),"GenJet"))
+      className="TopTree::TRootGenJet";
+    else if (strstr(ObjName.c_str(),"MCParticles"))
+      className="TopTree::TRootMCParticle";
+    else if (strstr(ObjName.c_str(),"NPGenEvent"))
+      className="TopTree::TRootNPGenEvent";
+    else if (strstr(ObjName.c_str(),"GenEvent"))
+      className="TopTree::TRootGenEvent";
+    else if (strstr(ObjName.c_str(),"Muon"))
+      className="TopTree::TRootMuon";
+    else if (strstr(ObjName.c_str(),"Electron"))
+      className="TopTree::TRootElectron";
+    else if (strstr(ObjName.c_str(),"TCMET"))
+      className="TopTree::TRootMET";
+    else if (strstr(ObjName.c_str(),"CaloMET"))
+      className="TopTree::TRootCaloMET";
+    else if (strstr(ObjName.c_str(),"PFMET"))
+      className="TopTree::TRootPFMET";
+    else if (strstr(ObjName.c_str(),"MET"))
+      className="TopTree::TRootMET";
+    else if (strstr(ObjName.c_str(),"MHT"))
+      className="TopTree::TRootMHT";
+    else if (strstr(ObjName.c_str(),"PrimaryVertex"))
+      className="TopTree::TRootVertex";     
+    
+    //cout << "  Loaded Branch " << className << " " << ObjName.substr(0,position) << endl;
+
+    //runinfos
+      
+    runTree->GetEvent(0);
+      
+    //if (fileID == 0 && verbosity > 3 && runInfos->hltInputTag() != "")
+    //	cout << "- " << "TopTree::TRootRun" << setw(5) << " -> " << "\"" << runInfos->hltInputTag() << "\""  << endl;
+    
+    // making some tests with the branch
+    
+    //Declartion of Branches and TClonesArray
+    
+    TClonesArray* objects = new TClonesArray(className.c_str(), 0);
+
+    eventTree->SetBranchStatus((ObjName.substr(0,position)+'*').c_str(),1);
+    eventTree->SetBranchAddress((ObjName.substr(0,position)+'*').c_str(),&objects);
+
+    if (objects) {
+      if (verbosity > 1) cout << "  Loaded Branch " << className << " " << ObjName.substr(0,position) << endl;
+     
+      arrays[ObjName.substr(0,position)]=std::pair<std::string,TClonesArray*>(className,objects);
+      
+      } else if (verbosity > 1) cerr << "  ERROR loading Branch " << className << " " << ObjName.substr(0,position) << endl;
+  
+  }
+
+  if (verbosity > 1) cout << "eventTree->GetEntries(): " << eventTree->GetEntries() << endl;
+  if (verbosity > 1) cout << "runTree->GetEntries(): " << runTree->GetEntries() << endl;
+
+  if (verbosity > 0) cout << "Looping over " << eventTree->GetEntries() << " events " << endl;
+  for(unsigned int ievt=0; ievt<eventTree->GetEntries(); ievt++) {
+    //for(unsigned int ievt=0; ievt<8000; ievt++) {
+    eventTree->GetEvent(ievt);
+    runTree->GetEvent(0);
+
+    if (verbosity > 0 && ievt % 1000 == 0) std::cout<<"Processing the "<<ievt<<"th event." << flush<<"\r";
+
+    for (std::map<std::string, std::pair<std::string,TClonesArray*> >::const_iterator it=arrays.begin(); it != arrays.end(); ++it) {
+
+      //cout << it->first << " " << it->second.first << " " << it->second.second->GetEntries() << endl;
+      
+      //
+      // define global plots we want to see
+      //
+      
+      string hist="";
+      
+      hist="_size";
+      if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),100,0,100);
+      
+      for (unsigned int o=0; o<it->second.second->GetEntries(); o++) {
+	
+	// because c++ it not flexible we need to write the elaborate code below.... Sorry:'(
+	
+	//
+	//
+	// Dear object developer:) You can find some sample code here to add plots so all your changes will happen below this comment.....
+	//
+	//
+	
+	//
+	// HLT
+	//
+	
+	/* NO IMPLEMENTATION YET */
+	
+	//
+	// PRIMARY VERTEX
+	//
+	
+	if (strstr(it->second.first.c_str(),"TopTree::TRootVertex")) {
+	  
+	  if(o==0) histos[it->first+"_size"]->Fill(it->second.second->GetEntries());
+	  
+	}
+	
+	//
+	// GenEvent
+	//
+	
+	else if (strstr(it->second.first.c_str(),"TopTree::TRootGenEvent") || strstr(it->second.first.c_str(),"TopTree::TRootNPGenEvent")) {
+	  
+	  if(o==0) histos[it->first+"_size"]->Fill(it->second.second->GetEntries());
+	  
+	}
+	
+	//
+	// MCParticles
+	//
+	
+	else if (strstr(it->second.first.c_str(),"TopTree::TRootMCParticle")) {
+	  
+	  if(o==0) histos[it->first+"_size"]->Fill(it->second.second->GetEntries());
+	  
+	}
+	
+	//
+	// JETS
+	//
+	
+	// now putting all jet types together, these can be addressed seperately in the future if needed
+	else if (strstr(it->second.first.c_str(),"TopTree::TRootGenJet") || strstr(it->second.first.c_str(),"TopTree::TRootCaloJet") || strstr(it->second.first.c_str(),"TopTree::TRootPFJet") || strstr(it->second.first.c_str(),"TopTree::TRootJPTJet") ){
+	  
+	  if(o==0) histos[it->first+"_size"]->Fill(it->second.second->GetEntries());
+	  
+	  // some local plots
+	  hist="_pt";
+	  if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),250,0,500);
+	  hist="_eta";
+	  if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),300,-6,6);	  
+	  hist="_phi";
+	  if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),250,-5,5);
+	  
+	  histos[it->first+"_pt"]->Fill(((TRootJet*)it->second.second->At(o))->Pt());
+	  histos[it->first+"_eta"]->Fill(((TRootJet*)it->second.second->At(o))->Eta());
+	  histos[it->first+"_phi"]->Fill(((TRootJet*)it->second.second->At(o))->Phi());
+	  
+	}
+	
+	// 
+	// MET
+	//
+	
+	// now putting all met types together, these can be addressed seperately in the future if needed
+	else if (strstr(it->second.first.c_str(),"TopTree::TRootCaloMET") || strstr(it->second.first.c_str(),"TopTree::TRootPFMET") || strstr(it->second.first.c_str(),"TopTree::TRootMET") ){
+	  
+	  if(o==0) histos[it->first+"_size"]->Fill(it->second.second->GetEntries());
+	  
+	  // some local plots
+	  hist="_Et";
+	  if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),250,0,500);
+	  
+	  histos[it->first+"_Et"]->Fill(((TRootMET*)it->second.second->At(o))->Et());
+	  
+	  
+	}
+	
+	//
+	// MUON
+	//
+	
+	else if (strstr(it->second.first.c_str(),"TopTree::TRootMuon")){
+	  
+	  if(o==0) histos[it->first+"_size"]->Fill(it->second.second->GetEntries());
+	  
+	  // some local plots
+	  hist="_pt";
+	  if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),250,0,500);
+	  hist="_eta";
+	  if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),300,-6,6);	  
+	  hist="_phi";
+	  if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),250,-5,5);
+	  
+	  histos[it->first+"_pt"]->Fill(((TRootMuon*)it->second.second->At(o))->Pt());
+	  histos[it->first+"_eta"]->Fill(((TRootMuon*)it->second.second->At(o))->Eta());
+	  histos[it->first+"_phi"]->Fill(((TRootMuon*)it->second.second->At(o))->Phi());
+	  
+	}
+	
+	//
+	// ELECTRON
+	//
+	
+	else if (strstr(it->second.first.c_str(),"TopTree::TRootElectron")){
+	  
+	  if(o==0) histos[it->first+"_size"]->Fill(it->second.second->GetEntries());
+	  
+	  // some local plots
+	  hist="_pt";
+	  if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),250,0,500);
+	  hist="_eta";
+	  if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),300,-6,6);	  
+	  hist="_phi";
+	  if (histos.find(it->first+hist) == histos.end()) histos[it->first+hist]=new TH1F((it->first+hist).c_str(),(it->first).c_str(),250,-5,5);
+	  
+	  histos[it->first+"_pt"]->Fill(((TRootElectron*)it->second.second->At(o))->Pt());
+	  histos[it->first+"_eta"]->Fill(((TRootElectron*)it->second.second->At(o))->Eta());
+	  histos[it->first+"_phi"]->Fill(((TRootElectron*)it->second.second->At(o))->Phi());
+	  
+	}
+	
+      }
+      
+      //delete it->second.second;
+    }
+    
+  } cout << endl; // end of event loop
 
   if(verbosity>1) cout <<"Writing the plots" << endl;
 
@@ -446,5 +452,6 @@ int main(int argc, char *argv[]){
   }
 
   if(verbosity>1) cout<<"End of the Macro"<<endl;
-  
+
+  return 0;
 }
