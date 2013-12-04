4f435f47d8106033510c7cfd75cc8daf85a12aca
Taejeong <monttj@gmail.com>
2013-12-04 22:10:19 +0100

    make file needed for skim

---
+++ src/LinkDef.h
@@ -0,0 +1,84 @@
+#ifdef __CINT__
+#include "../interface/TRootRun.h"
+#include "../interface/TRootEvent.h"
+#include "../interface/TRootParticle.h"
+#include "../interface/TRootMCParticle.h"
+#include "../interface/TRootJet.h"
+#include "../interface/TRootJPTJet.h"
+#include "../interface/TRootCaloJet.h"
+#include "../interface/TRootGenJet.h"
+#include "../interface/TRootPFJet.h"
+#include "../interface/TRootMuon.h"
+#include "../interface/TRootElectron.h"
+#include "../interface/TRootLepton.h"
+#include "../interface/TRootPhoton.h"
+#include "../interface/TRootMET.h"
+#include "../interface/TRootCaloMET.h"
+#include "../interface/TRootPFMET.h"
+#include "../interface/TRootTrackMET.h"
+#include "../interface/TRootGenEvent.h"
+#include "../interface/TRootGenTop.h"
+#include "../interface/TRootNPGenEvent.h"
+#include "../interface/TRootSpinCorrGen.h"
+#include "../interface/TRootVertex.h"
+#include "../interface/TRootHLTInfo.h"
+#else
+#include "../interface/TRootRun.h"
+#include "../interface/TRootEvent.h"
+#include "../interface/TRootParticle.h"
+#include "../interface/TRootMCParticle.h"
+#include "../interface/TRootJet.h"
+#include "../interface/TRootJPTJet.h"
+#include "../interface/TRootCaloJet.h"
+#include "../interface/TRootGenJet.h"
+#include "../interface/TRootPFJet.h"
+#include "../interface/TRootMuon.h"
+#include "../interface/TRootElectron.h"
+#include "../interface/TRootLepton.h"
+#include "../interface/TRootPhoton.h"
+#include "../interface/TRootMET.h"
+#include "../interface/TRootCaloMET.h"
+#include "../interface/TRootPFMET.h"
+#include "../interface/TRootTrackMET.h"
+#include "../interface/TRootGenEvent.h"
+#include "../interface/TRootGenTop.h"
+#include "../interface/TRootNPGenEvent.h"
+#include "../interface/TRootSpinCorrGen.h"
+#include "../interface/TRootVertex.h"
+#include "../interface/TRootHLTInfo.h"
+#endif
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class TopTree::TRootRun;
+#pragma link C++ class TopTree::TRootEvent;
+#pragma link C++ class TopTree::TRootParticle;
+#pragma link C++ class TopTree::TRootMCParticle;
+#pragma link C++ class TopTree::TRootJet;
+#pragma link C++ class TopTree::TRootJPTJet;
+#pragma link C++ class TopTree::TRootCaloJet;
+#pragma link C++ class TopTree::TRootGenJet;
+#pragma link C++ class TopTree::TRootPFJet;
+#pragma link C++ class TopTree::TRootMuon;
+#pragma link C++ class TopTree::TRootElectron;
+#pragma link C++ class TopTree::TRootLepton;
+#pragma link C++ class TopTree::TRootPhoton;
+#pragma link C++ class TopTree::TRootMET;
+#pragma link C++ class TopTree::TRootCaloMET;
+#pragma link C++ class TopTree::TRootPFMET;
+#pragma link C++ class TopTree::TRootTrackMET;
+#pragma link C++ class TopTree::TRootGenEvent;
+#pragma link C++ class TopTree::TRootGenTop;
+#pragma link C++ class TopTree::TRootNPGenEvent;
+#pragma link C++ class TopTree::TRootSpinCorrGen;
+#pragma link C++ class TopTree::TRootVertex;
+#pragma link C++ class TopTree::TRootHLTInfo;
+
+#pragma link C++ struct TopTree::triggeredObject;
+
+#endif
+
+
