c129f47ecf2b76d7a3de7dfafc83d47a3b454b0c
Denys Lontkovskyi <denys.lontkovskyi@cern.ch>
2017-03-08 19:40:45 +0100

    Add one reReco test file

--- prod/TOPTREE_fromminiAOD.py
+++ prod/TOPTREE_fromminiAOD.py
@@ -144,6 +144,7 @@ process.options = cms.untracked.PSet(
 #Test sample MC (synch ex)
 process.source = cms.Source("PoolSource",
 fileNames = cms.untracked.vstring('root://xrootd-cms.infn.it///store/mc/RunIISummer16MiniAODv2/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/0693E0E7-97BE-E611-B32F-0CC47A78A3D8.root'),
+#fileNames = cms.untracked.vstring('root://cms-xrd-global.cern.ch//store/data/Run2016G/DoubleMuon/MINIAOD/03Feb2017-v1/100000/00182C13-EEEA-E611-8897-001E675A6C2A.root'),
 skipEvents = cms.untracked.uint32(0)
 )
 
